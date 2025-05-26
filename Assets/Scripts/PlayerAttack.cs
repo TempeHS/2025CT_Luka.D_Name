ba223423cbd3ee1550190f6e0c4a2379ceb6de65
Embaii <luka.davies1@education.nsw.gov.au>
2025-05-26 10:38:11 +1000

    commit

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private GameObject attackArea = defeault;
+    private bool attacking = false;
+    privare floattimeToAttack = 0.25f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
